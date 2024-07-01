c36c35634d5a272d0fb007118df5b1ce1d3c4741
Ahmedbo9 <ahmed.benothman@live.fr>
2024-07-01 15:03:23 -0400

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+#configure the AWS provider
+provider "aws" {
+    region = "us-west-1"
+    profile = "terraform-user"
+}
+#storing the state file in s3 bucket
+terraform {
+  backend "s3" {
+    bucket = "abo9-terraform-state"
+    key    = "terraform.tfstate.dev"
+    region = "us-east-1"
+    profile = "terraform-user"
+
+  }
+}
